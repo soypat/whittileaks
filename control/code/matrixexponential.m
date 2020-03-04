9a0c6766b03a46e79df9868de676980d9c9be276
soypat <graded.sp@gmail.com>
2020-03-04 12:42:33 -0300

    fixes

--- control/code/matrixexponential.m
+++ control/code/matrixexponential.m
@@ -1,10 +1,9 @@
 A = rand(3);
-t1 = 0.5; 
-fprintf('e^(At)=');disp(exp(A*t1));
-fprintf('e^(At)=');disp(expt(t1,1e4,A));
+t0 = 0.5; 
+fprintf('e^(At)=');disp(expt(t0,1e5,A));
 function y = expt(t,n,A)
-    y = ones(size(A,1));
+    y = eye(size(A));
     for i=1:n
-        y = y + (A*t).^i/factorial(i);
+        y = y + (A*t)^i/factorial(i);
     end
 end
