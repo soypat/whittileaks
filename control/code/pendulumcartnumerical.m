3291ca1d463287fae0af3ac53645468cf692a2f2
soypat <graded.sp@gmail.com>
2020-05-12 14:41:30 -0300

    fix code control lqr

--- control/code/pendulumcartnumerical.m
+++ control/code/pendulumcartnumerical.m
@@ -1,5 +1,15 @@
 clear all;clc;
-% Constantes fisicos
+%% Seleccion de problema
+s = -1; % s=1 pendulo normal.  s=-1  pendulo invertido
+%% Puntos de Equilibrio y condiciones iniciales
+if s == 1
+    x_eq = [0 0 0 0]';
+    x0 = [-3 1 0 0];
+elseif s==-1
+    x_eq = [0 pi 0 0]';
+    x0 = [-3 pi+.1 0 0]';
+end
+%% Constantes fisicos
 L = 2;
 g = 10;
 m = 1;
@@ -8,7 +18,7 @@ k = 0; % constante de resorte
 b = 0; % Friccion de pendulo
 d = 0; % Friccion de carro
 
-%% Se define el sistema no-lineal
+%% Sistema NO-LINEAL
 F = @(X,u,t) -k*X(1) - d*X(3) + u(1); % Fuerza sobre carro
 % X = [x; q; Dx; Dq] donde q es el angulo (medido desde vertical, q=pi corresponde para pendulo invertido)
 D2x = @(X,u,t)( F(X,u,t) + m*L*X(4)^2*sin(X(2)) +m*g*sin(X(2))*cos(X(2)) +...
@@ -18,7 +28,7 @@ D2q= @(X,u,t) (-F(X,u,t)*cos(X(2)) - m*L*X(4)^2*sin(X(2))*cos(X(2))- ...
 Y = @(X,u,t) [X(3); X(4); D2x(X,u,t); D2q(X,u,t)];
 
 %% SISTEMA LINEAL   X = [x; q; Dx; Dq]
-s = 1; % s=1 pendulo normal.  s=-1  pendulo invertido
+
 A = [0      0             1      0;
      0      0             0      1;
    -k/M    m*g/M        -d/M     s*b/L/M;
@@ -31,28 +41,18 @@ p = size(B,2); % number of inputs
 C = zeros(n);
 C(2,2) = 1; % medimos el angulo
 D = zeros(n,p); % no feedthrough
-% Funcion costo LQR
+%% Funcion costo (LQR)
 Q = diag([1 10 1 100]);
 R =1e-4;
 Kr = lqr(A,B,Q,R);
-% Peturbaciones (Vd) y ruido (Vn)
+%% Peturbaciones (Vd) y ruido (Vn) para filtro kalman (no implementado)
 Vd = .1*eye(n);
 Vn = 1*eye(n);
 Baum = [B Vd 0*B]; % matrices B y D del sistema aumentado
 Daum = [D 0*D Vn];
 
 sys = ss(A,B,C,D);
-Kf = (lqr(A',C',Vd,Vn))'; % Ideal Kalman filter for system
-% Kf = lqe(A,Vd,C,Vd,Vn);
-return
-if s == 1
-    x_eq = [0 0 0 0]';
-    x0 = [-3 1 0 0];
-elseif s==-1
-    x_eq = [1 pi 0 0]';
-    x0 = [-3 pi+.1 0 0]';
-end
-
+%% Solucion temporal con runge kutta O4
 ts = .01; % Tiempo de sampleo de nuestras mediciones
 tend = 10; % Finalizacion de la simulacion
 tsrk = ts/10; % paso de tiempo del sistema no lineal
@@ -87,33 +87,3 @@ for it = 1:anistep:Nt
     ax = plotCartPend(ax,Xt(:,it),L,M,m);
     drawnow
 end
-return
-figure(2)
-comet(q,Dq)
-% plot(tspan,Xt(2,:))
-
-return
-%% RK4 time
-tend = 20; % end of simulation time
-Nt = 1000; % time steps  
-dt = tend/(Nt-1);
-tspan = linspace(0,tend,Nt);
-Xt = zeros(4,Nt);
-%% Condiciones de borde
-Xt(1,1) = 0;  % x
-Xt(2,1) = pi; % q pendulo invertido => q=pi
-Xt(3,1) = 0;  % Dx
-Xt(4,1) = 0;  % Dq
-%% Resolucion Runge Kutta orden 4
-for it = 1:Nt-1
-    t=tspan(it);
-    X = Xt(:,it);
-    forz = F(X,t);
-    a = Y(X,forz);
-    b = Y(X+dt/2.*a,forz);
-    c = Y(X+dt/2.*b,forz);
-    d = Y(X+dt.*c,forz);
-    Xt(:,it+1) = X + dt/6*(a+2*(b+c)+d);
-end
-x = Xt(1,:); q = Xt(2,:); Dx=Xt(3,:); Dq = Xt(4,:);
-plot(tspan,q)
